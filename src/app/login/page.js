37685fd8d643241620a041a2e76ff0801de2c36a
hamid647 <67857318+hamid647@users.noreply.github.com>
2025-06-03 12:51:50 +0500

    edited login page

---
+++ src/app/login/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Login() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      margin: 0
+    }}>
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/app/signup/page.js
@@ -0,0 +1,15 @@
+'use client';
+
+export default function Signup() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '100vh',
+      margin: 0
+    }}>
+      <h1>Signup</h1>
+    </div>
+  );
+}
